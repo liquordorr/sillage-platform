e4c588491a00786bacbc980fb754c4962d17a1c6
liquordorr <eli20060103@gmail.com>
2026-03-23 16:57:12 +0900

    Create SillageApplication.java

---
+++ sillage-backend/src/main/java/kr/sillage/SillageApplication.java
@@ -0,0 +1,11 @@
+package kr.sillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SillageApplication.class, args);
+    }
+}
